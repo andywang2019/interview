8e186c386bc61677553b804e6d7724cdf187980b
andywang2019 <andywang9201@gmail.com>
2025-09-05 22:56:46 -0500

    yuhao lesson2

---
+++ code/java/lesson_1/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        //while (true) {
+            String ss = "hello";
+
+        String str = new String("hello");
+        String str2=str.replace('l','k');
+        System.out.println(str2);
+        System.out.println(str2==str);
+            StringBuffer s2 = new StringBuffer("hello");
+
+            fun(s2);
+            System.out.println(s2);
+            int a=10;
+            fun2(a);
+
+            System.out.println(a);
+      //  }
+    }
+
+
+    //pass by reference
+    public static void fun(StringBuffer ss){
+         ss.append("world");
+    }
+    //pass by value : int float,
+    public static void fun2(int a){
+        a=a+2;
+        System.out.println(a);
+    }
+
+
+}
